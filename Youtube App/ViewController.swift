f1973fd14cae77cf8abc707da6306f0f9d940413
Ronnil Okoro <ronnilokoro@Ronnils-MacBook-Air.local>
2020-09-03 14:16:39 -0700

    initial xcode project creation

---
+++ Youtube App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube App
+//
+//  Created by Ronnil Okoro on 03/09/2020.
+//  Copyright © 2020 Emmanuel Okoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
