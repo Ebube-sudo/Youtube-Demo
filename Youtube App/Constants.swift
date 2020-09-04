98bbf8307779bff638981ea437de5e81552d0519
Ronnil Okoro <ronnilokoro@Ronnils-MacBook-Air.local>
2020-09-04 01:32:00 -0700

    Fixed error

---
+++ Youtube App/Constants.swift
@@ -0,0 +1,55 @@
+//
+//  Constants.swift
+//  Youtube App
+//
+//  Created by Ronnil Okoro on 03/09/2020.
+//  Copyright © 2020 Emmanuel Okoro. All rights reserved.
+//
+
+import Foundation
+
+
+import Foundation
+
+
+struct Constants {
+    
+    static var API_KEY = "AIzaSyBma7et84gkkaTZo-mAb_QDN7wLeTnJc80"
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
